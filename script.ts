db8c03105c012ce33a8cd44582267b9e0fcd275e
DasunNirmal <dasun200301@gmail.com>
2025-01-19 19:58:28 +0530

    Item context path added

--- script.ts
+++ script.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import prismaCustomerRoutes from "./routes/prisma-customer-routes";
+import prismaItemRoutes from "./routes/prisma-item-routes";
 
 const app = express();
 
 app.use(express.json());
 app.use('/customer',prismaCustomerRoutes);
+app.use('/item',prismaItemRoutes);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
